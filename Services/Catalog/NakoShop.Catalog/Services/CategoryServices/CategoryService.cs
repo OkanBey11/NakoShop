06bbe2a2d6a7d9ee2a4ff633e996b45d996dfe32
OkanBey11 <okangoztok@hotmail.com>
2025-02-10 23:20:41 +0300

    Category Services Created

---
+++ Services/Catalog/NakoShop.Catalog/Services/CategoryServices/CategoryService.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using MongoDB.Driver;
+using NakoShop.Catalog.Dtos.CategoryDtos;
+using NakoShop.Catalog.Entities;
+using NakoShop.Catalog.Settings;
+
+namespace NakoShop.Catalog.Services.CategoryServices
+{
+    public class CategoryService : ICategoryServices
+    {
+        private readonly IMongoCollection<Category> _categoriesCollection;
+        private readonly IMapper _mapper;
+
+        public CategoryService(IMapper mapper, IDatabaseSettings databaseSettings)
+        {
+            var client = new MongoClient(databaseSettings.ConnectionString);
+            var database = client.GetDatabase(databaseSettings.DatabaseName);
+            _categoriesCollection = database.GetCollection<Category>(databaseSettings.CategoryCollectionName);
+            _mapper = mapper;
+        }
+
+        public async Task CreateCategoryAsync(CreateCategoryDto createCategoryDto)
+        {
+            var value = _mapper.Map<Category>(createCategoryDto);
+            await _categoriesCollection.InsertOneAsync(value);
+        }
+
+        public async Task DeleteCategoryAsync(string id)
+        {
+            await _categoriesCollection.DeleteOneAsync(x => x.CategoryID == id);
+        }
+
+        public async Task<List<ResultCategoryDto>> GetAllCategories()
+        {
+            var values = await _categoriesCollection.Find(x => true).ToListAsync();
+            return _mapper.Map<List<ResultCategoryDto>>(values);
+        }
+
+        public async Task<GetByIdCategoryDto> GetByIdCategoryAsync(string id)
+        {
+            var values = await _categoriesCollection.Find<Category>(x => x.CategoryID == id).FirstOrDefaultAsync();
+            return _mapper.Map<GetByIdCategoryDto>(values);
+        }
+
+        public async Task UpdateCategoryAsync(UpdateCategoryDto updateCategoryDto)
+        {
+            var values = _mapper.Map<Category>(updateCategoryDto);
+            await _categoriesCollection.FindOneAndReplaceAsync(x => x.CategoryID == updateCategoryDto.CategoryID, values);
+        }
+    }
+}
---
+++ Services/Catalog/NakoShop.Catalog/Services/CategoryServices/ICategoryServices.cs
@@ -0,0 +1,13 @@
+﻿using NakoShop.Catalog.Dtos.CategoryDtos;
+
+namespace NakoShop.Catalog.Services.CategoryServices
+{
+    public interface ICategoryServices
+    {
+        Task<List<ResultCategoryDto>> GetAllCategories();
+        Task CreateCategoryAsync(CreateCategoryDto createCategoryDto);
+        Task UpdateCategoryAsync(UpdateCategoryDto updateCategoryDto);
+        Task DeleteCategoryAsync(string id);
+        Task<GetByIdCategoryDto> GetByIdCategoryAsync(string id);
+    }
+}
