1f518cf283a76f9ffb83c44ff7c9f4881568db71
OkanBey11 <okangoztok@hotmail.com>
2025-02-12 17:34:08 +0300

    Api test and map fixed

--- Services/Catalog/NakoShop.Catalog/Mapping/GeneralMapping.cs
+++ Services/Catalog/NakoShop.Catalog/Mapping/GeneralMapping.cs
@@ -24,7 +24,7 @@ namespace NakoShop.Catalog.Mapping
             CreateMap<ProductDetail, ResultProductDetailDto>().ReverseMap();
             CreateMap<ProductDetail, CreateProductDetailDto>().ReverseMap();
             CreateMap<ProductDetail, UpdateProductDetailDto>().ReverseMap();
-            CreateMap<ProductDetail, GetByIdProductDto>().ReverseMap();
+            CreateMap<ProductDetail, GetByIdProductDetailDto>().ReverseMap();
 
             CreateMap<ProductImage, ResultProductImageDto>().ReverseMap();
             CreateMap<ProductImage, CreateProductImageDto>().ReverseMap();
