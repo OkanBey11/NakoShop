0e1a37068b3fe6338c708478d359615b29cefb37
OkanBey11 <okangoztok@hotmail.com>
2025-02-14 11:40:02 +0300

    Address Command and Hadler Created

---
+++ Services/Order/Core/NakoShop.Order.Application/Features/CQRS/Handlers/AddressHandlers/CreateAddressCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using NakoShop.Order.Application.Features.CQRS.Commands.AddressCommands;
+using NakoShop.Order.Application.Interfaces;
+using NakoShop.Order.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NakoShop.Order.Application.Features.CQRS.Handlers.AddressHandlers
+{
+    public class CreateAddressCommandHandler
+    {
+        private readonly IRepository<Adddress> _repository;
+
+        public CreateAddressCommandHandler(IRepository<Adddress> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(CreateAddressCommand createAddressCommand)
+        {
+            await _repository.CreateAsync(new Adddress
+            {
+                City = createAddressCommand.City,
+                Detail = createAddressCommand.Detail,
+                District = createAddressCommand.District,
+                UserId = createAddressCommand.UserId,
+            });
+        }
+    }
+}
