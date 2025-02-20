0d0f3265f6c154d5c4bd4d540d59c533758aeb38
OkanBey11 <okangoztok@hotmail.com>
2025-02-20 17:55:50 +0300

    JWT added

---
+++ IdentityServer/NakoShop.IdentityServer/Controllers/RegisterController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NakoShop.IdentityServer.Dtos;
+using NakoShop.IdentityServer.Models;
+using System.Threading.Tasks;
+using static IdentityServer4.IdentityServerConstants;
+
+namespace NakoShop.IdentityServer.Controllers
+{
+    [Authorize(LocalApi.PolicyName)]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegisterController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public RegisterController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UserRegister(UserRegisterDto userRegisterDto)
+        {
+            var values = new ApplicationUser()
+            {
+                UserName = userRegisterDto.Username,
+                Email = userRegisterDto.Email,
+                Name = userRegisterDto.Name,
+                Surname = userRegisterDto.Surname,
+            };
+            var result = await _userManager.CreateAsync(values, userRegisterDto.Password);
+            if(result.Succeeded)
+            {
+                return Ok("User added");
+            }
+            else
+            {
+                return BadRequest("user add fail");
+            }
+        }
+    }
+}
